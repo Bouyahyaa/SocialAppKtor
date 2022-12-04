0447e489f9fe38029028444493de0354b262aa88
MacBook Air <macbook@MacBook-Air-de-MacBook.local>
2022-12-04 17:07:16 +0100

    change base url

--- src/main/kotlin/com/bouyahya/util/Constants.kt
+++ src/main/kotlin/com/bouyahya/util/Constants.kt
@@ -1,7 +1,7 @@
 package com.bouyahya.util
 
 object Constants {
-    const val BASE_URL = "http://192.168.68.120:8080/"
+    const val BASE_URL = "http://192.168.1.12:8080/"
     const val PROFILE_PICTURE_PATH = "build/resources/main/static/profile_pictures/"
     const val DEFAULT_PROFILE_PICTURE_PATH = "${BASE_URL}profile_pictures/avatar.svg"
     const val EMAIL_CODE = "email_code"
